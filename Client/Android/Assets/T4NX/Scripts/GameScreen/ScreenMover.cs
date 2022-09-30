9bec8009fce45d499297fa025262697745a7aef1
SielsCore <d.sremski@gmail.com>
2022-09-30 14:24:02 +0200

    Update: refactorisation and improvements of T4NX scene

---
+++ Client/Android/Assets/T4NX/Scripts/GameScreen/ScreenMover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace T4NX
+{
+    public class ScreenMover : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            //DOTween
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
