e19d7d5008cc092b42875263f1c807678fdd0d2f
SielsCore <d.sremski@gmail.com>
2022-10-06 05:21:51 +0200

    Add/remove: refactored class

--- Client/Android/Assets/T4NX/Scripts/Factories/TankistFactory.cs
+++ Client/Android/Assets/T4NX/Scripts/Factories/TankistFactory.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TankerFactory : MonoBehaviour
+public class TankistFactory : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
