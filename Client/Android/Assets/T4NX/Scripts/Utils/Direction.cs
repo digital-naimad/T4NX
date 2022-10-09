4effdb2d74c2f22b117b572dd7047c860ce95dad
SielsCore <d.sremski@gmail.com>
2022-10-09 20:39:26 +0200

    Add: Direction utility enum

---
+++ Client/Android/Assets/T4NX/Scripts/Utils/Direction.cs
@@ -0,0 +1,25 @@
+namespace T4NX
+{
+    /// <summary>
+    /// Universal utility enum
+    /// </summary>
+    public enum Direction 
+    {
+        /// <summary>
+        /// UP
+        /// </summary>
+        North,
+        /// <summary>
+        /// DOWN
+        /// </summary>
+        South,
+        /// <summary>
+        /// LEFT
+        /// </summary>
+        West,
+        /// <summary>
+        /// RIGHT
+        /// </summary>
+        East
+    }
+}
