1faebd324da7bcc1ec8f894395474e1091ea7082
SielsCore <d.sremski@gmail.com>
2022-10-14 04:17:12 +0200

    Add: Battlefield controller

---
+++ Client/Android/Assets/T4NX/Scripts/Gameplay/Management/BattlefieldController.cs
@@ -0,0 +1,19 @@
+namespace T4NX
+{
+    public class BattlefieldController : MonoSingleton<BattlefieldController>
+    {
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
